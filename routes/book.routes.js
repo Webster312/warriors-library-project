febe095877493bb40b657660c34a87046f1e9199
Christoph Weber <WeberChristoph85@gmail.com>
2023-03-20 15:10:21 +0100

    "updated"

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,26 +1,69 @@
-const express = require('express');
-const Book = require('../models/Book.model');
+const express = require("express");
+const Book = require("../models/Book.model");
 const router = express.Router();
 
 // GET /books
 router.get("/books", (req, res, next) => {
-  
   Book.find()
-    .then( booksArr => {
-
+    .then((booksArr) => {
       const data = {
-        books: booksArr
+        books: booksArr,
       };
 
       res.render("books/books-list", data);
     })
-    .catch( e => {
+    .catch((e) => {
       console.log("error getting books from DB", e);
       next(e);
     });
+});
 
- 
+// Get / books / create
+router.get("/books/create", (req, res, next) => {
+  res.render("books/book-create");
+});
 
+
+
+
+// POST Books
+router.post("/books", (req, res, next) => {
+  console.log(req.body);
+
+  const bookDetails = {
+    title: req.body.title,
+    description: req.body.description,
+    author: req.body.author,
+    rating: req.body.rating,
+  };
+
+  Book.create(bookDetails)
+    .then( bookFromDB => {
+      res.redirect("/books");
+    })
+    .catch(e => {
+      console.log("error creating new book", e);
+      next(e);
+    });
+
+});
+
+
+
+
+
+//GET /books/:bookId
+router.get("/books/:bookId", (req, res, next) => {
+  const { bookId } = req.params;
+
+  Book.findById(bookId)
+    .then((bookDetails) => {
+      res.render("books/book-details", bookDetails);
+    })
+    .catch((e) => {
+      console.log("error getting book details from DB", e);
+      next(e);
+    });
 });
 
 module.exports = router;
